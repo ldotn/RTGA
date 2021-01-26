f192b06bfe63e3fbb6cf39eb915b2fc42091c03d
Santiago Pacheco <pachesantiago@gmail.com>
2021-01-26 10:39:10 -0300

    Just starting out

---
+++ HighLevelSim/ExecutionUnit.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <systemc.h>
+
+using namespace sc_core;
+
+struct ExecutionUnit : sc_module
+{
+	sc_in<int> codetable;
+	sc_out<int> current_ip;
+	sc_in<BlockIndex> current_block;
+	sc_in_clk clk;
+
+	SC_CTOR(ExecutionUnit)
+	{
+		SC_CTHREAD(Step, clk.pos());
+	}
+
+	void Step();
+private:
+	int mIP;
+
+	FetchUnit  mFetch;
+	DecodeUnit mDecode;
+	ALU        mALU;
+};
